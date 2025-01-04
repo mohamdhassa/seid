7f60eaf81ad55290291a2ada08142c748246963f
mohamdhassa <mohamddet@gmail.com>
2025-01-04 07:38:24 +0300

    .

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-// JavaScript
 document.addEventListener("DOMContentLoaded", () => {
     const sections = document.querySelectorAll("section");
-    const options = {
-        threshold: 0.1,
-    };
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("visible");
             } else {
-                entry.target.classList.remove("visible");
+                entry.target.classList.remove("visible"); // Remove visibility when scrolling out
             }
         });
-    }, options);
+    }, {
+        threshold: 0.2, // Trigger animations when 20% of the section is in view
+    });
 
     sections.forEach((section) => {
         observer.observe(section);
